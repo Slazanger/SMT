1e604d436f53c14db959699549f7aefacd4b4c42
Steve Sharp <steve@bitbaboon.net>
2019-12-17 23:34:01 +0000

    Missed file

--- UniverseControl.xaml.cs
+++ UniverseControl.xaml.cs
@@ -69,6 +69,8 @@ namespace SMT
 
 
         private VisualHost VH;
+        private VisualHost VHNames;
+        private VisualHost VHRegionNames;
 
 
 
@@ -88,7 +90,12 @@ namespace SMT
             universeZMax = 472860102256057000.0;
 
             VH = new VisualHost();
+            VHNames = new VisualHost();
+            VHRegionNames = new VisualHost();
             UniverseMainCanvas.Children.Add(VH);
+            //UniverseMainCanvas.Children.Add(VHNames);
+
+
 
 
             foreach (EVEData.System sys in EM.Systems)
@@ -156,8 +163,6 @@ namespace SMT
         public void ReDrawMap(bool FullRedraw = false)
         {
          
-
-            double Padding = -3;
             double textXOffset = 5;
             double textYOffset = 5;
 
@@ -168,6 +173,7 @@ namespace SMT
             Brush SysCol = new SolidColorBrush(Colors.Black);
             Brush ConstGateCol = new SolidColorBrush(Colors.Gray);
             Brush TextCol = new SolidColorBrush(Colors.DarkGray);
+            Brush RegionTextCol = new SolidColorBrush(Colors.Black);
             Brush GateCol = new SolidColorBrush(Colors.LightGray);
             Brush JBCol = new SolidColorBrush(Colors.Blue);
 
@@ -183,7 +189,39 @@ namespace SMT
 
             if(FullRedraw)
             {
- 
+
+
+                foreach (EVEData.MapRegion mr in EM.Regions)
+                {
+                    double X = (mr.RegionX - universeXMin) * scale; ;
+                    double Z = (universeDepth - (mr.RegionZ - universeZMin)) * scale;
+
+                    // Create an instance of a DrawingVisual.
+                    System.Windows.Media.DrawingVisual SystemTextVisual = new System.Windows.Media.DrawingVisual();
+
+                    // Retrieve the DrawingContext from the DrawingVisual.
+                    DrawingContext drawingContext = SystemTextVisual.RenderOpen();
+
+#pragma warning disable CS0618 // 'FormattedText.FormattedText(string, CultureInfo, FlowDirection, Typeface, double, Brush)' is obsolete: 'Use the PixelsPerDip override'
+                    // Draw a formatted text string into the DrawingContext.
+
+
+
+                    FormattedText ft = new FormattedText(mr.Name, CultureInfo.GetCultureInfo("en-us"), FlowDirection.LeftToRight, new Typeface("Verdana"), 60, RegionTextCol);
+                    ft.TextAlignment = TextAlignment.Center;
+
+                    drawingContext.DrawText(ft,new Point(X + textXOffset, Z + textYOffset) );
+#pragma warning enable CS0618 // 'FormattedText.FormattedText(string, CultureInfo, FlowDirection, Typeface, double, Brush)' is obsolete: 'Use the PixelsPerDip override'
+
+                    // Close the DrawingContext to persist changes to the DrawingVisual.
+                    drawingContext.Close();
+
+                    VHRegionNames.Children.Add(SystemTextVisual);
+
+                }
+
+
+
 
                 foreach (GateHelper gh in universeSysLinksCache)
                 {
@@ -192,15 +230,6 @@ namespace SMT
 
                     double X2 = (gh.to.ActualX - universeXMin) * scale;
                     double Y2 = (universeDepth - (gh.to.ActualZ - universeZMin)) * scale;
-/*
-                    Line sysLink = new Line();
-
-                    sysLink.X1 = (gh.from.ActualX - universeXMin) * scale;
-                    sysLink.Y1 = (universeDepth - (gh.from.ActualZ - universeZMin)) * scale;
-                    sysLink.X2 = (gh.to.ActualX - universeXMin) * scale;
-                    sysLink.Y2 = (universeDepth - (gh.to.ActualZ - universeZMin)) * scale;
-
-                */
                     Brush Col = GateCol;
 
                     if (gh.from.Region != gh.to.Region || gh.from.ConstellationID != gh.to.ConstellationID)
@@ -220,24 +249,6 @@ namespace SMT
                     drawingContext.Close();
 
                     VH.Children.Add(sysLinkVisual);
-
-
-
-                    /*
-                    if (gh.from.Region != gh.to.Region || gh.from.ConstellationID != gh.to.ConstellationID)
-                    {
-                        sysLink.Stroke = ConstGateCol;
-                    }
-                    else
-                    {
-                        sysLink.Stroke = GateCol;
-                    }
-                    sysLink.StrokeThickness = 1;
-                    Canvas.SetZIndex(sysLink, 19);
-                   // UniverseMainCanvas.Children.Add(sysLink);
-
-                */
-
                 }
 
             }
@@ -274,33 +285,6 @@ namespace SMT
                 drawingContext.Close();
 
                 VH.Children.Add(sysLinkVisual);
-
-
-
-                /*
-
-                                jbLink.X1 = (from.ActualX - universeXMin) * scale;
-                                jbLink.Y1 = (universeDepth - (from.ActualZ - universeZMin)) * scale;
-
-                                jbLink.X2 = (to.ActualX - universeXMin) * scale;
-                                jbLink.Y2 = (universeDepth - (to.ActualZ - universeZMin)) * scale;
-
-                                jbLink.StrokeThickness = 1;
-                                jbLink.Visibility = Visibility.Visible;
-
-                                jbLink.Stroke = JBCol;
-
-
-                                DoubleCollection dashes = new DoubleCollection();
-                                dashes.Add(1.0);
-                                dashes.Add(1.0);
-
-                                jbLink.StrokeDashArray = dashes;
-
-                                Canvas.SetZIndex(jbLink, 19);
-                              //  UniverseMainCanvas.Children.Add(jbLink);
-                            
-                            */
             }
 
 
@@ -331,16 +315,6 @@ namespace SMT
                 drawingContext.Close();
                 VH.Children.Add(systemShapeVisual);
 
-                /*
-                Shape systemShape = new Rectangle() { Height = 6, Width = 6 };
-                systemShape.Fill = SysCol;
-                Canvas.SetLeft(systemShape, Padding + X);
-                Canvas.SetTop(systemShape, Padding + Z);
-                Canvas.SetZIndex(systemShape, 20);
-                UniverseMainCanvas.Children.Add(systemShape);
-                */
-
-
                 // add text
 
 
@@ -364,7 +338,7 @@ namespace SMT
                 // Close the DrawingContext to persist changes to the DrawingVisual.
                 drawingContext.Close();
 
-                VH.Children.Add(SystemTextVisual);
+                VHNames.Children.Add(SystemTextVisual);
 
                 /*
                 Label sysText = new Label();
@@ -387,170 +361,202 @@ namespace SMT
 
         }
 
+        private void MainZoomControl_ZoomChanged(object sender, RoutedEventArgs e)
+        {
+            if(MainZoomControl.Zoom < 0.8)
+            {
+                if(UniverseMainCanvas.Children.Contains(VHNames))
+                {
+                    UniverseMainCanvas.Children.Remove(VHNames);
+                }
 
-                        /*
-
-                        private void OnPaintSurface(object sender, SkiaSharp.Views.Desktop.SKPaintSurfaceEventArgs e)
-                        {
-                            // the the canvas and properties
-                            var canvas = e.Surface.Canvas;
-
-                            double XScale = (15000) / universeWidth;
-                            double ZScale = (15000) / universeDepth;
-                            double scale = Math.Min(XScale, ZScale);
-
-                            float textXOffset = 10;
-                            float textYOffset = 10;
-
-                            float padding = 250.0f;
-
-                            canvas.Clear(SKColors.LightGray);
-
-
-                            Brush SysCol = new SolidColorBrush(Colors.Black);
-                            Brush ConstGateCol = new SolidColorBrush(Colors.Gray);
-                            Brush TextCol = new SolidColorBrush(Colors.DarkGray);
-                            Brush JBCol = new SolidColorBrush(Colors.Blue);
-
-
-                            var SysPaint = new SKPaint
-                            {
-                                TextSize = 12.0f,
-                                IsAntialias = true,
-                                Color = SKColors.Black,
-                                Style = SKPaintStyle.Fill,
-                                StrokeWidth = 2
-                            };
-
-                            var ConstGatePaint = new SKPaint
-                            {
-                                TextSize = 12.0f,
-                                IsAntialias = true,
-                                Color = SKColors.Gray,
-                                Style = SKPaintStyle.Fill,
-                                StrokeWidth = 2
-                            };
-
-                            var TextPaint = new SKPaint
-                            {
-                                TextSize = 12.0f,
-                                IsAntialias = true,
-                                Color = SKColors.DarkGray,
-                                Style = SKPaintStyle.Fill,
-                                StrokeWidth = 2
-                            };
-
-                            var JBPaint = new SKPaint
-                            {
-                                TextSize = 12.0f,
-                                IsAntialias = true,
-                                Color = SKColors.Blue,
-                                Style = SKPaintStyle.Fill,
-                                StrokeWidth = 2
-                            };
-
-                            var ZKBPaint = new SKPaint
-                            {
-                                TextSize = 12.0f,
-                                IsAntialias = true,
-                                Color = SKColors.Purple,
-                                Style = SKPaintStyle.Fill,
-                                StrokeWidth = 2
-                            };
-
-
-                            // paint in back to front order
+                if (!UniverseMainCanvas.Children.Contains(VHRegionNames))
+                {
+                    UniverseMainCanvas.Children.Add(VHRegionNames);
+                }
 
 
 
-                            Dictionary<string, int> ZKBBaseFeed = new Dictionary<string, int>();
-                            {
-                                foreach (EVEData.ZKillRedisQ.ZKBDataSimple zs in EM.ZKillFeed.KillStream)
-                                {
-                                    if (ZKBBaseFeed.Keys.Contains(zs.SystemName))
-                                    {
-                                        ZKBBaseFeed[zs.SystemName]++;
-                                    }
-                                    else
-                                    {
-                                        ZKBBaseFeed[zs.SystemName] = 1;
-                                    }
-                                }
-
-
-                                foreach (EVEData.System sys in EM.Systems)
-                                {
-                                    if (ZKBBaseFeed.Keys.Contains(sys.Name))
-                                    {
-                                        float ZKBValue = 10 + (ZKBBaseFeed[sys.Name] * 2);
-
-                                        float X = (float)((sys.ActualX - universeXMin) * scale);
-
-                                        // need to invert Z
-                                        float Y = (float)((universeDepth - (sys.ActualZ - universeZMin)) * scale);
-
-                                        X += padding;
-                                        Y += padding;
-
-                                        canvas.DrawCircle(X, Y, ZKBValue, ZKBPaint);
-
-
-                                        canvas.DrawText(sys.Name, X + textXOffset, Y + textYOffset, SysPaint);
-
-                                    }
-                                }
-                            }
-
-
-
-
-
-                            foreach (EVEData.System sys in EM.Systems)
-                            {
-                                double size = 8.0;
-                                double halfSize = size / 2.0;
-
-                                float X = (float)( (sys.ActualX - universeXMin) * scale);
-
-                                // need to invert Z
-                                float Y = (float)((universeDepth - (sys.ActualZ - universeZMin)) * scale);
-
-                                X += padding;
-                                Y += padding;
-
-                                canvas.DrawCircle(X, Y, 9, SysPaint);
-
-
-                                canvas.DrawText(sys.Name, X + textXOffset, Y + textYOffset, SysPaint);
-                            }
-
-                            foreach (GateHelper gh in universeSysLinksCache)
-                            {
-
-                                float X1 = (float)((gh.from.ActualX - universeXMin) * scale);
-                                float Y1 = (float)((universeDepth - (gh.from.ActualZ - universeZMin)) * scale);
-
-                                float X2 = (float)((gh.to.ActualX - universeXMin) * scale);
-                                float Y2 = (float)((universeDepth - (gh.to.ActualZ - universeZMin)) * scale);
-
-                                X1 += padding;
-                                Y1 += padding;
-                                X2 += padding;
-                                Y2 += padding;
-
-
-
-
-
-                                if (gh.from.Region != gh.to.Region || gh.from.ConstellationID != gh.to.ConstellationID)
-                                {
-                                    canvas.DrawLine(X1, Y1, X2, Y2, ConstGatePaint); 
-                                }
-                                else
-                                {
-                                   canvas.DrawLine(X1, Y1, X2, Y2, SysPaint);
-                                }
-                            }
-                        } */
             }
+            else
+            {
+                if (!UniverseMainCanvas.Children.Contains(VHNames))
+                {
+                    UniverseMainCanvas.Children.Add(VHNames);
+                }
+
+                if (UniverseMainCanvas.Children.Contains(VHRegionNames))
+                {
+                    UniverseMainCanvas.Children.Remove(VHRegionNames);
+                }
+
+            }
+        }
+
+
+        /*
+
+        private void OnPaintSurface(object sender, SkiaSharp.Views.Desktop.SKPaintSurfaceEventArgs e)
+        {
+            // the the canvas and properties
+            var canvas = e.Surface.Canvas;
+
+            double XScale = (15000) / universeWidth;
+            double ZScale = (15000) / universeDepth;
+            double scale = Math.Min(XScale, ZScale);
+
+            float textXOffset = 10;
+            float textYOffset = 10;
+
+            float padding = 250.0f;
+
+            canvas.Clear(SKColors.LightGray);
+
+
+            Brush SysCol = new SolidColorBrush(Colors.Black);
+            Brush ConstGateCol = new SolidColorBrush(Colors.Gray);
+            Brush TextCol = new SolidColorBrush(Colors.DarkGray);
+            Brush JBCol = new SolidColorBrush(Colors.Blue);
+
+
+            var SysPaint = new SKPaint
+            {
+                TextSize = 12.0f,
+                IsAntialias = true,
+                Color = SKColors.Black,
+                Style = SKPaintStyle.Fill,
+                StrokeWidth = 2
+            };
+
+            var ConstGatePaint = new SKPaint
+            {
+                TextSize = 12.0f,
+                IsAntialias = true,
+                Color = SKColors.Gray,
+                Style = SKPaintStyle.Fill,
+                StrokeWidth = 2
+            };
+
+            var TextPaint = new SKPaint
+            {
+                TextSize = 12.0f,
+                IsAntialias = true,
+                Color = SKColors.DarkGray,
+                Style = SKPaintStyle.Fill,
+                StrokeWidth = 2
+            };
+
+            var JBPaint = new SKPaint
+            {
+                TextSize = 12.0f,
+                IsAntialias = true,
+                Color = SKColors.Blue,
+                Style = SKPaintStyle.Fill,
+                StrokeWidth = 2
+            };
+
+            var ZKBPaint = new SKPaint
+            {
+                TextSize = 12.0f,
+                IsAntialias = true,
+                Color = SKColors.Purple,
+                Style = SKPaintStyle.Fill,
+                StrokeWidth = 2
+            };
+
+
+            // paint in back to front order
+
+
+
+            Dictionary<string, int> ZKBBaseFeed = new Dictionary<string, int>();
+            {
+                foreach (EVEData.ZKillRedisQ.ZKBDataSimple zs in EM.ZKillFeed.KillStream)
+                {
+                    if (ZKBBaseFeed.Keys.Contains(zs.SystemName))
+                    {
+                        ZKBBaseFeed[zs.SystemName]++;
+                    }
+                    else
+                    {
+                        ZKBBaseFeed[zs.SystemName] = 1;
+                    }
+                }
+
+
+                foreach (EVEData.System sys in EM.Systems)
+                {
+                    if (ZKBBaseFeed.Keys.Contains(sys.Name))
+                    {
+                        float ZKBValue = 10 + (ZKBBaseFeed[sys.Name] * 2);
+
+                        float X = (float)((sys.ActualX - universeXMin) * scale);
+
+                        // need to invert Z
+                        float Y = (float)((universeDepth - (sys.ActualZ - universeZMin)) * scale);
+
+                        X += padding;
+                        Y += padding;
+
+                        canvas.DrawCircle(X, Y, ZKBValue, ZKBPaint);
+
+
+                        canvas.DrawText(sys.Name, X + textXOffset, Y + textYOffset, SysPaint);
+
+                    }
+                }
+            }
+
+
+
+
+
+            foreach (EVEData.System sys in EM.Systems)
+            {
+                double size = 8.0;
+                double halfSize = size / 2.0;
+
+                float X = (float)( (sys.ActualX - universeXMin) * scale);
+
+                // need to invert Z
+                float Y = (float)((universeDepth - (sys.ActualZ - universeZMin)) * scale);
+
+                X += padding;
+                Y += padding;
+
+                canvas.DrawCircle(X, Y, 9, SysPaint);
+
+
+                canvas.DrawText(sys.Name, X + textXOffset, Y + textYOffset, SysPaint);
+            }
+
+            foreach (GateHelper gh in universeSysLinksCache)
+            {
+
+                float X1 = (float)((gh.from.ActualX - universeXMin) * scale);
+                float Y1 = (float)((universeDepth - (gh.from.ActualZ - universeZMin)) * scale);
+
+                float X2 = (float)((gh.to.ActualX - universeXMin) * scale);
+                float Y2 = (float)((universeDepth - (gh.to.ActualZ - universeZMin)) * scale);
+
+                X1 += padding;
+                Y1 += padding;
+                X2 += padding;
+                Y2 += padding;
+
+
+
+
+
+                if (gh.from.Region != gh.to.Region || gh.from.ConstellationID != gh.to.ConstellationID)
+                {
+                    canvas.DrawLine(X1, Y1, X2, Y2, ConstGatePaint); 
+                }
+                else
+                {
+                   canvas.DrawLine(X1, Y1, X2, Y2, SysPaint);
+                }
+            }
+        } */
+    }
         }
