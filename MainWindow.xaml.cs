3a333f14ae14d6e0b42b8fbde07a2f4ce43b6863
Steve Sharp <steve@bitbaboon.net>
2020-10-13 21:42:01 +0100

    SMT Version update

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -26,7 +26,7 @@ namespace SMT
     /// </summary>
     public partial class MainWindow : Window
     {
-        public const string SMT_VERSION = "SMT_093";
+        public const string SMT_VERSION = "SMT_094";
         public static MainWindow AppWindow;
         private LogonWindow logonBrowserWindow;
 
@@ -50,7 +50,7 @@ namespace SMT
 
             InitializeComponent();
 
-            Title = "SMT (Andals Landing : " + SMT_VERSION + ")";
+            Title = "SMT (Blue Falcon Patrol : " + SMT_VERSION + ")";
 
             CheckGitHubVersion();
 
