2fd7e5229bef235ed8bc81810706ea1cab51c830
Steve Sharp <steve@bitbaboon.net>
2021-10-25 19:53:53 +0100

    updated version string

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -26,7 +26,7 @@ namespace SMT
     /// </summary>
     public partial class MainWindow : Window
     {
-        public const string SMT_VERSION = "SMT_105";
+        public const string SMT_VERSION = "SMT_106";
         public static MainWindow AppWindow;
         private LogonWindow logonBrowserWindow;
 
@@ -52,7 +52,7 @@ namespace SMT
 
             InitializeComponent();
 
-            Title = "SMT (A new beginning : " + SMT_VERSION + ")";
+            Title = "SMT (drop down, increase speed, change direction : " + SMT_VERSION + ")";
 
             CheckGitHubVersion();
 
