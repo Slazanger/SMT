a189b7a19a91ee25126cc6d62c63f927e8e0a98b
Steve Sharp <steve@bitbaboon.net>
2022-05-02 13:20:13 +0100

    remove deprecated webrequest from ZKill Feed

--- EVEData/ZKillRedisQ.cs
+++ EVEData/ZKillRedisQ.cs
@@ -10,6 +10,7 @@ using System.Linq;
 using System.Net;
 using System.Windows;
 using System.Windows.Threading;
+using System.Net.Http;
 
 namespace SMT.EVEData
 {
@@ -68,72 +69,54 @@ namespace SMT.EVEData
         private void zkb_DoWork(object sender, DoWorkEventArgs e)
         {
             string redistURL = @"https://redisq.zkillboard.com/listen.php";
-
-            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(redistURL);
-            request.Method = WebRequestMethods.Http.Get;
-            request.Timeout = 60000;
-            request.UserAgent = VerString;
-            request.KeepAlive = true;
-            request.Proxy = null;
-            HttpWebResponse response;
-
+            string strContent = string.Empty;
             try
             {
-                response = request.GetResponse() as HttpWebResponse;
+                HttpClient hc = new HttpClient();
+                var response = hc.GetAsync(redistURL).Result;
+                if(response.IsSuccessStatusCode)
+                {
+                    strContent = response.Content.ReadAsStringAsync().Result;
+                }
             }
-            catch (Exception)
+            catch
             {
                 e.Result = -1;
                 return;
             }
 
-            Stream responseStream = response.GetResponseStream();
 
-            using (StreamReader sr = new StreamReader(responseStream))
+            ZKBData.ZkbData z = ZKBData.ZkbData.FromJson(strContent);
+            if (z.Package != null)
             {
-                try
+                ZKBDataSimple zs = new ZKBDataSimple();
+                zs.KillID = long.Parse(z.Package.KillId.ToString());
+                zs.VictimAllianceID = long.Parse(z.Package.Killmail.Victim.AllianceId.ToString());
+                zs.VictimCharacterID = long.Parse(z.Package.Killmail.Victim.CharacterId.ToString());
+                zs.VictimCorpID = long.Parse(z.Package.Killmail.Victim.CharacterId.ToString());
+                zs.SystemName = EveManager.Instance.GetEveSystemNameFromID(z.Package.Killmail.SolarSystemId);
+                if (zs.SystemName == string.Empty)
                 {
-                    // Need to return this response
-                    string strContent = sr.ReadToEnd();
-
-                    ZKBData.ZkbData z = ZKBData.ZkbData.FromJson(strContent);
-                    if (z.Package != null)
-                    {
-                        ZKBDataSimple zs = new ZKBDataSimple();
-                        zs.KillID = long.Parse(z.Package.KillId.ToString());
-                        zs.VictimAllianceID = long.Parse(z.Package.Killmail.Victim.AllianceId.ToString());
-                        zs.VictimCharacterID = long.Parse(z.Package.Killmail.Victim.CharacterId.ToString());
-                        zs.VictimCorpID = long.Parse(z.Package.Killmail.Victim.CharacterId.ToString());
-                        zs.SystemName = EveManager.Instance.GetEveSystemNameFromID(z.Package.Killmail.SolarSystemId);
-                        if (zs.SystemName == string.Empty)
-                        {
-                            zs.SystemName = z.Package.Killmail.SolarSystemId.ToString();
-                        }
-
-                        zs.KillTime = z.Package.Killmail.KillmailTime.ToLocalTime();
-                        string shipID = z.Package.Killmail.Victim.ShipTypeId.ToString();
-                        if (EveManager.Instance.ShipTypes.Keys.Contains(shipID))
-                        {
-                            zs.ShipType = EveManager.Instance.ShipTypes[shipID];
-                        }
-                        else
-                        {
-                            zs.ShipType = "Unknown (" + shipID + ")";
-                        }
-
-                        zs.VictimAllianceName = EveManager.Instance.GetAllianceName(zs.VictimAllianceID);
-
-                        Application.Current.Dispatcher.Invoke((Action)(() =>
-                        {
-                            KillStream.Insert(0, zs);
-                        }));
-                    }
+                    zs.SystemName = z.Package.Killmail.SolarSystemId.ToString();
                 }
-                catch
+
+                zs.KillTime = z.Package.Killmail.KillmailTime.ToLocalTime();
+                string shipID = z.Package.Killmail.Victim.ShipTypeId.ToString();
+                if (EveManager.Instance.ShipTypes.Keys.Contains(shipID))
                 {
-                    e.Result = -1;
-                    return;
+                    zs.ShipType = EveManager.Instance.ShipTypes[shipID];
                 }
+                else
+                {
+                    zs.ShipType = "Unknown (" + shipID + ")";
+                }
+
+                zs.VictimAllianceName = EveManager.Instance.GetAllianceName(zs.VictimAllianceID);
+
+                Application.Current.Dispatcher.Invoke((Action)(() =>
+                {
+                    KillStream.Insert(0, zs);
+                }));
             }
 
             e.Result = 0;
