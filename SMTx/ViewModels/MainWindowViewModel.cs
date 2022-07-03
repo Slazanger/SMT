1e5d95d750ac2cdced91e59fe040ed3da78f6e09
Steve Sharp <steve@bitbaboon.net>
2022-07-03 19:41:58 +0100

    Initial SMTx

---
+++ SMTx/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace SMTx.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting => "Welcome to Avalonia!";
+    }
+}
---
+++ SMTx/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace SMTx.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
