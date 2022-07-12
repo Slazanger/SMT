923f6e2edf45b0038458885dd59932099a970d58
Steve Sharp <steve@bitbaboon.net>
2022-07-12 16:55:44 +0100

    updated palceholder

--- SMTx/Models/Eve.cs
+++ SMTx/Models/Eve.cs
@@ -6,7 +6,8 @@ using System.Threading.Tasks;
 
 namespace SMTx.Models
 {
-    internal class Eve
+    // this is the main container class for interacting with the EVE/ESI Manager
+    public class Eve
     {
     }
 }
