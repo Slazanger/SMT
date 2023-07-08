2e8560ac37be83379f7578d5d76a9fd4cf6f6d83
lorensghassan <125946757+lorensghassan@users.noreply.github.com>
2023-07-08 01:20:41 -0700

    Implemented Game Log Panel, along with font size property. Updated Avalonia Dependencies to version 11. Implemented UI changes based on update from Avalonia Preview to Version 11.

--- SMTx/ViewModels/Tools/GameLogFeedViewModel.cs
+++ SMTx/ViewModels/Tools/GameLogFeedViewModel.cs
@@ -1,8 +1,64 @@
 ﻿using Dock.Model.Mvvm.Controls;
+using SMT.EVEData;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 
 namespace SMTx.ViewModels.Tools
 {
     public class GameLogFeedViewModel : Tool
     {
+        private double m_textSize;
+
+        public double TextSize
+        {
+            get
+            {
+                if (m_textSize <= 0)
+                    return 12;
+                else
+                    return m_textSize;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    SetProperty(ref m_textSize, value);
+                }
+            }
+        }
+
+        public ObservableCollection<GameLogData> GameLogData { get; init; }
+
+        public GameLogFeedViewModel()
+        {
+            GameLogData = new ObservableCollection<GameLogData>();
+        }
+
+        public GameLogFeedViewModel(IEnumerable<GameLogData> gameLogData)
+        {
+            GameLogData = new ObservableCollection<GameLogData>(gameLogData);
+        }
+
+        public void ClearGameLog()
+        {
+            GameLogData.Clear();
+        }
+
+        public void AddToGameLog(GameLogData logEntry)
+        {
+            GameLogData.Add(logEntry);
+        }
+
+        public void AddToGameLog(IEnumerable<GameLogData> logEntries)
+        {
+            if (logEntries != null)
+            {
+                foreach (var logEntry in logEntries)
+                {
+                    GameLogData.Add(logEntry);
+                }
+            }
+        }
     }
 }
