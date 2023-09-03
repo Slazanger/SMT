0f9c9b21f211268005f667ac584b6ec2d4314443
Slazanger <steve@shockmonkey.net>
2023-09-03 16:12:53 +0100

    set title on load

--- SMTx/Views/MainWindow.axaml.cs
+++ SMTx/Views/MainWindow.axaml.cs
@@ -1,6 +1,8 @@
 ﻿using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using ESI.NET.Models.FactionWarfare;
+using SMT.EVEData;
 
 namespace SMTx.Views;
 
@@ -13,5 +15,8 @@ public partial class MainWindow : Window
 #if DEBUG
         this.AttachDevTools();
 #endif
+
+        // Set the windows title 
+        Title = $"SMT - {SMT.EVEData.EveAppConfig.SMT_TITLE} ({SMT.EVEData.EveAppConfig.SMT_VERSION})";
     }
 }
