43ba092cd4a3e215354800f90b2b76e4f3de7e6d
Steve Sharp <steve@bitbaboon.net>
2023-10-13 16:17:18 +0100

    updated version info

--- EVEData/EveAppConfig.cs
+++ EVEData/EveAppConfig.cs
@@ -23,12 +23,12 @@ namespace SMT.EVEData
         /// <summary>
         /// SMT Version Tagline
         /// </summary>
-        public const string SMT_TITLE = "Status ? Best not to ask..";
+        public const string SMT_TITLE = "Zarzakh.. just a super highway";
 
         /// <summary>
         /// SMT Version
         /// </summary>
-        public const string SMT_VERSION = "SMT_124";
+        public const string SMT_VERSION = "SMT_125";
 
         /// <summary>
         /// Folder to store all of the data from
