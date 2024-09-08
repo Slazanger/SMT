5a0cfc2aca1a11b7dd5b3b1402d20808dfac6828
Steve Sharp <steve@bitbaboon.net>
2024-09-08 19:01:28 +0100

    updated version number to 1.35

--- EVEData/EveAppConfig.cs
+++ EVEData/EveAppConfig.cs
@@ -23,12 +23,12 @@ namespace SMT.EVEData
         /// <summary>
         /// SMT Version Tagline
         /// </summary>
-        public const string SMT_TITLE = "Equinoctial .";
+        public const string SMT_TITLE = "Tractor fishing time";
 
         /// <summary>
         /// SMT Version
         /// </summary>
-        public const string SMT_VERSION = "SMT_134";
+        public const string SMT_VERSION = "SMT_135";
 
         /// <summary>
         /// Folder to store all of the data from
