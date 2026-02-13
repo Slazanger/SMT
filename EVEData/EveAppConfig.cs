003264fc8818c05463fc43580f12b955f87c7b9b
Slazanger <steve@shockmonkey.net>
2026-02-13 23:57:16 +0000

    package updates ESI.net update Version Update

--- EVEData/EveAppConfig.cs
+++ EVEData/EveAppConfig.cs
@@ -21,12 +21,12 @@ namespace SMT.EVEData
         /// <summary>
         /// SMT Version Tagline
         /// </summary>
-        public const string SMT_TITLE = "Winter Migration Time";
+        public const string SMT_TITLE = "A new space";
 
         /// <summary>
         /// SMT Version
         /// </summary>
-        public const string SMT_VERSION = "SMT_144";
+        public const string SMT_VERSION = "SMT_145";
 
         /// <summary>
         /// Folder to store all of the data from
